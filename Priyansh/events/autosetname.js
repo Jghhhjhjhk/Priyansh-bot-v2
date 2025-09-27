234cafc43cc549e7c669784d9e8edfb30860e66c
Jghhhjhjhk <ckerh63@gmail.com>
2025-09-27 07:34:55 +0100

    Update autosetname.js

--- Priyansh/events/autosetname.js
+++ Priyansh/events/autosetname.js
@@ -2,27 +2,30 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-	description: "Automatically set new member nicknames"
+	credits: "┋ֆ⑉󰟵-BOT-亗‣ᴟ➟mʀasۧۛiɭٰ.🛸󱢏",
+	description: "تعيين ألقاب تلقائية للأعضاء الجدد"
 };
 
 module.exports.run = async function({ api, event, Users }) {
-const { threadID } = event;
-var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
+	const { threadID } = event;
+	var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId);
 	for (let idUser of memJoin) {
-		const { readFileSync, writeFileSync } = global.nodemodule["fs-extra"];
-		const { join } = global.nodemodule["path"]
-		const pathData = join("./modules/commands","cache", "autosetname.json");
+		const { readFileSync } = global.nodemodule["fs-extra"];
+		const { join } = global.nodemodule["path"];
+		const pathData = join("./modules/commands", "cache", "autosetname.json");
 		var dataJson = JSON.parse(readFileSync(pathData, "utf-8"));
 		var thisThread = dataJson.find(item => item.threadID == threadID) || { threadID, nameUser: [] };
-		if (thisThread.nameUser.length == 0) return 
+		
+		if (thisThread.nameUser.length == 0) return;
 		if (thisThread.nameUser.length != 0) {  
-		var setName = thisThread.nameUser[0] 
-		await new Promise(resolve => setTimeout(resolve, 1000));
-		var namee1 = await api.getUserInfo(idUser)
-        var namee = namee1[idUser].name
-		api.changeNickname(`${setName} ${namee}`, threadID, idUser);
+			var setName = thisThread.nameUser[0];
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			var namee1 = await api.getUserInfo(idUser);
+			var namee = namee1[idUser].name;
+			api.changeNickname(`${setName} ${namee}`, threadID, idUser);
+			
+			// رسالة للترحيب مع اسم البوت
+			api.sendMessage(`✅ تم تعيين لقب مؤقت للعضو الجديد (${namee}) بواسطة ┋ֆ⑉󰟵-BOT-亗‣ᴟ➟mʀasۧۛiɭٰ.🛸󱢏`, threadID);
 		} 
 	}	
-	return api.sendMessage(`Set a temporary nickname for the new member`, threadID, event.messageID)
 }
