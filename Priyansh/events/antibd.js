5970ffd67b40ac3cff8d0d166c6f4a61b7c7bde7
Jghhhjhjhk <ckerh63@gmail.com>
2025-09-27 07:43:05 +0100

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-  description: "Against changing Bot's nickname"
+  credits: "┋ֆ⑉󰟵-BOT-亗‣ᴟ➟mʀasۧۛiɭٰ.🛸󱢏",
+  description: "منع تغيير لقب البوت"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
@@ -12,9 +12,18 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     var { BOTNAME, ADMINBOT } = global.config;
     var { nickname } = await Threads.getData(threadID, botID);
     var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
+    
+    if (logMessageData.participant_id == botID 
+        && author != botID 
+        && !ADMINBOT.includes(author) 
+        && logMessageData.nickname != nickname) {
+        
+        api.changeNickname(nickname, threadID, botID);
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐`}, threadID);
+
+        return api.sendMessage(
+          { body: `${info.name} ⚠️ لا يمكنك تغيير لقب البوت 😼\n✅ فقط المدير: https://www.facebook.com/profile.php?id=100000555481981 🖐` },
+          threadID
+        );
     }  
-        }
+}
